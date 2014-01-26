8c5274a2d76301a30ffaf3903a0e9e5f4db0437a
simpleframework <cknet@126.com>
2014-01-26 14:06:07 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/mvc/common/UrlsCache.java
+++ src/main/java/net/simpleframework/mvc/common/UrlsCache.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import net.simpleframework.common.object.ObjectEx;
 import net.simpleframework.mvc.AbstractMVCPage;
+import net.simpleframework.mvc.PageParameter;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -14,9 +15,34 @@ import net.simpleframework.mvc.AbstractMVCPage;
  */
 public abstract class UrlsCache extends ObjectEx {
 
-	public Class<? extends AbstractMVCPage> getUrl(final String key) {
-		return urls.get(key);
+	public String getUrl(final PageParameter pp, final Class<? extends AbstractMVCPage> mClass) {
+		return getUrl(pp, mClass, null);
 	}
 
-	protected final Map<String, Class<? extends AbstractMVCPage>> urls = new ConcurrentHashMap<String, Class<? extends AbstractMVCPage>>();
+	public String getUrl(final PageParameter pp, final Class<? extends AbstractMVCPage> mClass,
+			final String params) {
+		return AbstractMVCPage.url(getPageClass(mClass.getName()), params);
+	}
+
+	public Class<? extends AbstractMVCPage> getPageClass(final String key) {
+		return classMapping.get(key);
+	}
+
+	protected void put(final Class<? extends AbstractMVCPage> val) {
+		put(val, val);
+	}
+
+	protected void put(final Class<? extends AbstractMVCPage> key,
+			final Class<? extends AbstractMVCPage> val) {
+		classMapping.put(key.getName(), val);
+	}
+
+	protected void put(final String key, final Class<? extends AbstractMVCPage> val) {
+		classMapping.put(key, val);
+	}
+
+	private final Map<String, Class<? extends AbstractMVCPage>> classMapping;
+	{
+		classMapping = new ConcurrentHashMap<String, Class<? extends AbstractMVCPage>>();
+	}
 }
