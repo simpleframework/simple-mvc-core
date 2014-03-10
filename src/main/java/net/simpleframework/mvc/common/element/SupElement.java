4d81f2b36d17f90674b7cba7ac423e42a40d87d2
simpleframework <cknet@126.com>
2014-03-10 10:13:15 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/mvc/common/element/SupElement.java
+++ src/main/java/net/simpleframework/mvc/common/element/SupElement.java
@@ -9,7 +9,7 @@ package net.simpleframework.mvc.common.element;
 public class SupElement extends AbstractTagElement<SupElement> {
 
 	public static SupElement num(final int num) {
-		return new SupElement(num).addStyle("margin-left: 2px;");
+		return new SupElement(num).addStyle("margin-left: 2px; margin-top: 2px;");
 	}
 
 	private boolean highlight;
