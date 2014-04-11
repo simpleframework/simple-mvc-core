c1258430e7e8af0eb05241860eb4c69e5d79a61b
simpleframework <cknet@126.com>
2014-04-11 14:10:07 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/mvc/AbstractMVELTemplatePage.java
+++ src/main/java/net/simpleframework/mvc/AbstractMVELTemplatePage.java
@@ -17,80 +17,33 @@ import net.simpleframework.ctx.script.MVEL2Template.INamedTemplate;
  */
 public abstract class AbstractMVELTemplatePage extends AbstractMVCPage {
 
-	/**
-	 * 创建mvel预定义好的模板
-	 * 
-	 * html写法：$includeNamed{'key'}
-	 * 
-	 * @return
-	 */
-	protected NamedTemplate createNamedTemplates(final PageParameter pp) {
-		return null;
-	}
-
-	@Override
-	public IForward forward(final PageParameter pp) {
-		getNamedTemplate(pp);
-		return super.forward(pp);
-	}
-
-	protected NamedTemplate getNamedTemplate(final PageParameter pp) {
-		NamedTemplate nt = (NamedTemplate) pp.getRequestAttr("NamedTemplate");
-		if (nt == null) {
-			pp.setRequestAttr("NamedTemplate", nt = createNamedTemplates(pp));
-		}
-		return nt;
-	}
-
 	@Override
 	protected String replaceExpr(final PageParameter pp, final InputStream htmlStream,
 			final Map<String, Object> variables) throws IOException {
-		final NamedTemplate nt = getNamedTemplate(pp);
 		return MVEL2Template.replace(variables, super.replaceExpr(pp, htmlStream, variables),
-				nt == null ? null : nt);
+				new INamedTemplate() {
+					@Override
+					public Set<String> keySet() {
+						return templates.keySet();
+					}
+
+					@Override
+					public String get(final String key) {
+						return getMVELNamedTemplate(key);
+					}
+				});
 	}
 
-	public static class NamedTemplate implements INamedTemplate {
-		private final Map<String, TemplateVal> templates = new HashMap<String, TemplateVal>();
-
-		private final PageParameter pp;
-
-		public NamedTemplate(final PageParameter pp) {
-			this.pp = pp;
-		}
-
-		@Override
-		public String get(final String key) {
-			final TemplateVal val = templates.get(key);
-			return val != null ? val.getVal(pp) : null;
-		}
-
-		@Override
-		public Set<String> keySet() {
-			return templates.keySet();
-		}
-
-		public NamedTemplate add(final String key, final Class<? extends AbstractMVCPage> pageClass) {
-			// + "?referer=" + pp.getRequestURI()
-			templates.put(key, new TemplateVal(pageClass));
-			return this;
+	public void addMVELNamedTemplate(final PageParameter pp, final String key,
+			final Class<? extends AbstractMVCPage> pageClass) {
+		if (pageClass != null) {
+			templates.put(key, pp.includeUrl(pageClass));
 		}
 	}
 
-	static class TemplateVal {
-		Class<? extends AbstractMVCPage> pageClass;
-
-		String val;
-
-		TemplateVal(final Class<? extends AbstractMVCPage> pageClass) {
-			this.pageClass = pageClass;
-		}
-
-		String getVal(final PageParameter pp) {
-			if (val == null) {
-				val = pp.includeUrl(pageClass);
-			}
-			return val;
-		}
+	public String getMVELNamedTemplate(final String key) {
+		return templates.get(key);
 	}
+
+	private final Map<String, String> templates = new HashMap<String, String>();
 }
