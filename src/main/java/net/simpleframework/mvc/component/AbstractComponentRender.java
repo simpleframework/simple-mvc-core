d573475c620985c642d822fdccbc14a87bfd8bea
CHENK <CHENK@CHENK-PC>
2015-12-16 13:41:47 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/mvc/component/AbstractComponentRender.java
+++ src/main/java/net/simpleframework/mvc/component/AbstractComponentRender.java
@@ -1,5 +1,6 @@
 package net.simpleframework.mvc.component;
 
+import net.simpleframework.common.StringUtils;
 import net.simpleframework.common.object.ObjectEx;
 import net.simpleframework.common.th.NotImplementedException;
 import net.simpleframework.common.web.HttpUtils;
@@ -26,6 +27,31 @@ public abstract class AbstractComponentRender extends ObjectEx implements ICompo
 
 	public static abstract class ComponentJavascriptRender extends AbstractComponentRender implements
 			IComponentJavascriptRender {
+	}
+
+	public static abstract class ComponentBaseActionJavascriptRender extends
+			ComponentJavascriptRender {
+
+		protected abstract String getParams(ComponentParameter cp);
+
+		protected abstract String getActionPath(ComponentParameter cp);
+
+		@Override
+		public String getJavascriptCode(final ComponentParameter cp) {
+			final StringBuilder sb = new StringBuilder();
+			sb.append("var dc = function() { $Loading.hide(); };");
+			sb.append("$Loading.show();");
+			sb.append("var params=\"").append(StringUtils.blank(getParams(cp))).append("\";");
+			ComponentRenderUtils.appendParameters(sb, cp, "params");
+			sb.append("params = params.addParameter(arguments[0]);");
+			sb.append("new Ajax.Request('").append(getActionPath(cp)).append("', {");
+			sb.append("postBody: params,");
+			sb.append("onComplete: function(req) {");
+			sb.append("try { $call(req.responseText); } finally { dc(); }");
+			sb.append("}, onException: dc, onFailure: dc");
+			sb.append("});");
+			return ComponentRenderUtils.genActionWrapper(cp, sb.toString());
+		}
 	}
 
 	public static abstract class ComponentHtmlRender extends AbstractComponentRender implements
