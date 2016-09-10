0050f199c59939aff94082ca0ac870b94def66fc
CHENK <CHENK@CHENK-PC>
2016-09-10 12:49:45 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/mvc/PageRequestResponse.java
+++ src/main/java/net/simpleframework/mvc/PageRequestResponse.java
@@ -42,7 +42,8 @@ import eu.bitwalker.useragentutils.UserAgent;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class PageRequestResponse extends ObjectEx implements IMVCSettingsAware {
